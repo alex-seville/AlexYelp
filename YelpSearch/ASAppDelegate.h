d18356cb75bc1e8c3388cbbc7ae58f0d99e340dc
seville <seville@yahoo-inc.com>
2014-03-20 19:52:22 -0700

    Initial Commit

---
+++ YelpSearch/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  YelpSearch
+//
+//  Created by Alexander Seville on 3/20/14.
+//  Copyright (c) 2014 Alexander Seville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpSearchTests/YelpSearchTests.m
@@ -0,0 +1,34 @@
+//
+//  YelpSearchTests.m
+//  YelpSearchTests
+//
+//  Created by Alexander Seville on 3/20/14.
+//  Copyright (c) 2014 Alexander Seville. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YelpSearchTests : XCTestCase
+
+@end
+
+@implementation YelpSearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
